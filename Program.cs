f1558e068f642c1e898c31738163ea8309af63c4
Olesya <olesyaspb.ru@mail.ru>
2023-05-24 09:19:39 +0300

    написано решение 4 задания

--- Program.cs
+++ Program.cs
@@ -194,12 +194,159 @@ PrintArray(matrixC);
 
 
 
-// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+//которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+/* 
+//1-й вариант
+
+int deep1 = InputInt("Введите размерность 1: ");
+int deep2 = InputInt("Введите размерность 2: ");
+int deep3 = InputInt("Введите размерность 3: ");
+int countNums = 100;
+
+if (deep1 * deep2 * deep3 > countNums)
+{
+    Console.Write("Массив слишком большой");
+    return;
+}
+
+int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+
+for (int i = 0; i < resultNums.GetLength(0); i++)
+{
+    for (int j = 0; j < resultNums.GetLength(1); j++)
+    {
+        for (int k = 0; k < resultNums.GetLength(2); k++)
+        {
+            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,,] Create3DMassive(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+    int[] values = new int[countNums];
+    int num
+     = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num
+        ++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[valueIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+*/
+// 2-й вариант
+/*
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void Print3DArr (int[,,] arr3D)
+{
+  for (int i = 0; i < arr3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < arr3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={arr3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void Gen3DArray(int[,,] arr3D)
+{
+  int[] temp = new int[arr3D.GetLength(0) * arr3D.GetLength(1) * arr3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < arr3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < arr3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < arr3D.GetLength(2); z++)
+      {
+        arr3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите размер 3D массива X-Y-Z");
+int x = ReadData("Введите X: ");
+int y = ReadData("Введите Y: ");
+int z = ReadData("Введите Z: ");
+Console.WriteLine();
+
+int[,,] array3D = new int[x, y, z];
+
+Gen3DArray(array3D);
+
+Print3DArr(array3D);
+*/
+
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
